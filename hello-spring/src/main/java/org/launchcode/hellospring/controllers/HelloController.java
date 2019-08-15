b6274f9393bac300053dfbc64c13ae044c83c262
Lien Ryan <lienryanstl@gmail.com>
2019-08-15 14:40:46 -0500

    Add Hello World

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,20 +2,47 @@ package org.launchcode.hellospring.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
+
 public class HelloController {
     @RequestMapping(value = "")
     @ResponseBody
-    public String index(){
-        return "Hello World";
+    public String index(HttpServletRequest request){
+
+        String name = request.getParameter("name");
+
+        if (name ==null) {
+            name = "World";
+        }
+        return "Hello " +name;
+    }
+
+
+    //@RequestMapping(value = "hello", method= RequestMethod.GET)
+    //@ResponseBody
+
+
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name) {
+        return "hello "+ name;
+
+    }
+
+    @RequestMapping(value = "hello", method= RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        return "Hello "+ name;
+
     }
 
     @RequestMapping(value="goodbye")
-    @ResponseBody
     public String goodbye(){
-        return "Goodbye";
+        return "redirect:/";
     }
 }
--- hello.py
+++
@@ -1,9 +0,0 @@
-print("Hello")
-
-def main():
-    print('Hello world!')
-
-main()
-
-if __name__ == "__main__":
-    main()
